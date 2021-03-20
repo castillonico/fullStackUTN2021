9b76b36be8a1eb639b04429fabdb7a556b514ab6
castillonico <nico.a.castillo@gmail.com>
2021-03-20 16:12:54 -0300

    Tres Ejercicios Resueltos y Formateados

--- js/ejerciciosJS15-04-21.js
+++ js/ejerciciosJS15-04-21.js
@@ -1,11 +1,37 @@
 /*Ejercicio 01 */
-var elemento;
-elemento = document.querySelector("#holaMundo"); 
-const boton = document.querySelector("#cambioTexto");
-boton.addEventListener("click", cambiarTxt);
-function cambiarTxt(){
+var elemento1;
+elemento1 = document.querySelector("#holaMundo"); /*tomo el elemento holaMundo en una variable*/
+const boton = document.querySelector("#cambioTexto"); /*selecciono el boton, para agregar un listener*/
+boton.addEventListener("click", cambiarTxt); /*escucho por un click en el boton, para llamar a la funcion*/
 
-    elemento.innerHTML = "Hello JavaScriptWorld";
+function cambiarTxt(){ /*la funcion cambiara el texto en la variable seleccionada*/
+    elemento1.innerHTML = "Hello JavaScriptWorld!"; 
+    console.log("Resolución del Ejercicio 01");
+}
+
+/*Ejercicio 02*/
+const btnAumTam = document.querySelector("#cambioTamano");
+btnAumTam.addEventListener("click", aumentarTam);
+
+function aumentarTam(){
+    document.getElementById("txtACambiar").style.fontSize="40px";
+    console.log("Resolucion del Ejercicio 02");
+}
+
+
+/*Ejercicio 03*/
+const mostrar = document.querySelector("#btnMostrar"); /*selecciono el botón de mostrar*/
+mostrar.addEventListener("click", fncMostrar); /*agrego listener al botón*/
+const ocultar =document.querySelector("#btnOcultar"); /*selecciono el botón de ocultar*/
+ocultar.addEventListener("click", fncOcultar); /*agrego listener a ocultar*/
+
+function fncMostrar(){
+    console.log ("funcion mostrar");
+    document.getElementById("pMostrar").style.visibility = "visible";
+}
+
+function fncOcultar(){
+    console.log ("funcion ocultar");
+    document.getElementById("pOcultar").style.visibility = "hidden";
 
 }
-console.log(elemento); 

6f3fab1200660ef1ef35918f2f49805dd4062277
castillonico <nico.a.castillo@gmail.com>
2021-03-20 04:07:51 -0300

    primer Punto de Control

--- js/ejerciciosJS15-04-21.js
+++ js/ejerciciosJS15-04-21.js
@@ -1,1 +1,11 @@
 /*Ejercicio 01 */
+var elemento;
+elemento = document.querySelector("#holaMundo"); 
+const boton = document.querySelector("#cambioTexto");
+boton.addEventListener("click", cambiarTxt);
+function cambiarTxt(){
+
+    elemento.innerHTML = "Hello JavaScriptWorld";
+
+}
+console.log(elemento); 

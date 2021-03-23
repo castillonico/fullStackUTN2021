e70e4af9a4025f66ccc4ede5789b3c8273ae3304
castillonico <nico.a.castillo@gmail.com>
2021-03-23 18:52:36 -0300

    Funcionan Todos los Ejercicios

--- js/ejerciciosJS15-04-21.js
+++ js/ejerciciosJS15-04-21.js
@@ -1,16 +1,16 @@
 /*Ejercicio 01 */
-var elemento1;
-elemento1 = document.querySelector("#holaMundo"); /*tomo el elemento holaMundo en una variable*/
-const boton = document.querySelector("#cambioTexto"); /*selecciono el boton, para agregar un listener*/
-boton.addEventListener("click", cambiarTxt); /*escucho por un click en el boton, para llamar a la funcion*/
 
-function cambiarTxt(){ /*la funcion cambiara el texto en la variable seleccionada*/
+let boton = document.querySelector("#cambioTexto"); //selecciono el boton, para agregar un listener
+boton.addEventListener("click", cambiarTxt); //escucho por un click en el boton, para llamar a la funcion 
+
+function cambiarTxt(){ //la funcion cambiara el texto en la variable seleccionada 
+    let elemento1 = document.querySelector("#holaMundo"); //tomo el elemento holaMundo en una variable
     elemento1.innerHTML = "Hello JavaScriptWorld!"; 
     console.log("Resolución del Ejercicio 01");
 }
 
-/*Ejercicio 02*/
-const btnAumTam = document.querySelector("#cambioTamano");
+//Ejercicio 02 
+let btnAumTam = document.querySelector("#cambioTamano");
 btnAumTam.addEventListener("click", aumentarTam);
 
 function aumentarTam(){
@@ -19,28 +19,37 @@ function aumentarTam(){
 }
 
 
-/*Ejercicio 03*/
-const mostrar = document.querySelector("#btnMostrar"); /*selecciono el botón de mostrar*/
-mostrar.addEventListener("click", fncMostrar); /*agrego listener al botón*/
-const ocultar = document.querySelector("#btnOcultar"); /*selecciono el botón de ocultar*/
-ocultar.addEventListener("click", fncOcultar); /*agrego listener a ocultar*/
+//Ejercicio 03
+let mostrar = document.querySelector("#btnMostrar"); //selecciono el botón de mostrar 
+mostrar.addEventListener("click", fncMostrar); //agrego listener al botón 
+let ocultar = document.querySelector("#btnOcultar"); //selecciono el botón de ocultar 
+ocultar.addEventListener("click", fncOcultar); //agrego listener a ocultar 
 function fncMostrar(){
     console.log ("muestro parrafo oculto");
-    document.getElementById("pMostrar").style.visibility = "visible"; /*cambio el estilo CSS para mostrar el parrafo oculto*/
+    document.getElementById("pMostrar").style.visibility = "visible"; //cambio el estilo CSS para mostrar el parrafo oculto 
 }
 function fncOcultar(){
     console.log ("oculto un parrafo que estaba visible");
-    document.getElementById("pOcultar").style.visibility = "hidden"; /*cambio el estilo CSS para ocultar el parrafo visible*/
+    document.getElementById("pOcultar").style.visibility = "hidden"; //cambio el estilo CSS para ocultar el parrafo visible 
 }
 
-/*Ejercicio 04*/ 
-/* Es mostrar una URL que ya está resuelto en HTML */
+//Ejercicio 04 
+// Es mostrar una URL que ya está resuelto en HTML 
 
-/*Ejercicio 05*/ 
+//Ejercicio 05 
+
+let carga = document.getElementById("ingresar").addEventListener("click", cargaApellido);
+function cargaApellido(){
+    let ape = document.getElementById("apellido").value;
+    console.log(ape);
+    document.getElementById("ingresando").style.visibility = "visible";
+    let apeMostrar = document.getElementById("ingresando").innerHTML = ape;
+
+}
 
 
-/*Ejercicio 06 */
-const sobreEj06 =document.querySelector("#ejercicio06");
+//Ejercicio 06 
+let sobreEj06 =document.querySelector("#ejercicio06");
 sobreEj06.addEventListener("mouseover", cambioFondo);
 sobreEj06.addEventListener("mouseout", resetEj06);
 function cambioFondo(){
@@ -49,19 +58,19 @@ function cambioFondo(){
 function resetEj06(){
     document.getElementById("ejercicio06").style.background = "#fefefe";
 }
-const btnOscuro = document.querySelector("#dark");
+let btnOscuro = document.querySelector("#dark");
 btnOscuro.addEventListener("click", goDark); 
 function goDark () {
     console.log("cambio a modo oscuro");
     document.body.style.background = "#444645";
 }
-const btnAzul = document.querySelector("#goBlue"); 
+let btnAzul = document.querySelector("#goBlue"); 
 btnAzul.addEventListener("click", goAzul); 
 function goAzul(){
     console.log("pasamos a modo azul"); 
     document.body.style.background = "#96c2f1"; 
 }
-const btnResetear = document.querySelector("#resetear"); 
+let btnResetear = document.querySelector("#resetear"); 
 btnResetear.addEventListener("click", goClaro);
 function goClaro(){
     console.log("pasamos a Modo Claro"); 
@@ -69,18 +78,18 @@ function goClaro(){
 }
 
 
-/* Ejercicio 07 
-const txtMinus = document.getElementById("aMayus");
-const txtMayus = txtMinus.toUpperCase.call();
-console.log(txtMinus); */
+// Ejercicio 07 
+let txtMinus = document.getElementById("aMayus").innerText; 
+console.log(txtMinus);
+let txtMayus = txtMinus.toUpperCase(); 
+console.log(txtMayus);
 
-/* Ejercicio 08*/ 
-var cantA; 
-var aContar; 
-aContar = document.getElementById("conteo");
-console.log(aContar);
+
+//Ejercicio 08 
+let aContar = document.getElementById("conteo");
 contar();
 function contar(){
+    let cantA;
     cantA = aContar.getElementsByTagName('a').length;
     console.log (cantA);
 }
